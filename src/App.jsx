b240787fbd56c0d460eb642e76432e0357f92cb8
Hassaan Ahmad <62022834+hassaandev5@users.noreply.github.com>
2025-03-30 02:13:06 +0500

    Modified App.jsx, Made the Fetch Request on the Search Term and rendered the Movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import { BiMoviePlay } from "react-icons/bi";
+import Spinner from "./components/Spinner";
+import MovieDetail from "./components/MovieDetail";
+
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_BASE_URL = `http://www.omdbapi.com`;
+const API_OPTIONS = {
+  method: "GET",
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movie, setMovie] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovie = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = `${API_BASE_URL}/?t=${searchTerm}&apikey=${API_KEY}&`;
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error("Failed to fetch the movie");
+      }
+
+      const data = await response.json();
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovie("");
+        return;
+      }
+      console.log(data);
+
+      setMovie(data || "");
+    } catch (error) {
+      console.log("Error Fetching the movies");
+      setErrorMessage("Couldn't fetch movies please try again");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (searchTerm.trim() !== "") {
+      fetchMovie();
+    } else {
+      setMovie("");
+      setErrorMessage("");
+    }
+  }, [searchTerm]);
 
   return (
     <main className="min-h-screen bg-gray-50">
@@ -26,7 +73,20 @@ const App = () => {
         {/* Search Component */}
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
-      <h2>Search Term: {searchTerm}</h2>
+      <section className="searched-movies">
+        <h1 className="text-3xl">Movies</h1>
+
+        {/* {errorMessage && <p>{errorMessage}</p>} */}
+        {isLoading ? (
+          <Spinner />
+        ) : errorMessage ? (
+          <p>{errorMessage}</p>
+        ) : movie ? (
+          <MovieDetail movie={movie} />
+        ) : (
+          <p className="text-gray-500">Search for a movie to display details</p>
+        )}
+      </section>
     </main>
   );
 };
